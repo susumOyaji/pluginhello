5cf11a01d487dae1cfed34ed954469a324d779e6
susumOyaji <sumitomo0210@gmail.com>
2021-06-22 10:16:10 +0900

    up

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  String _batteryLevel = 'Unknown';
 
   @override
   void initState() {
@@ -23,11 +24,18 @@ class _MyAppState extends State<MyApp> {
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     String platformVersion;
+    String batteryLevel;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       platformVersion = await Pluginhello.platformVersion;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
+    }
+
+    try {
+      batteryLevel = await Pluginhello.batteryLevel;
+    } on PlatformException {
+      batteryLevel = 'Failed to get batteryLevel.';
     }
 
     // If the widget was removed from the tree while the asynchronous platform
@@ -37,6 +45,7 @@ class _MyAppState extends State<MyApp> {
 
     setState(() {
       _platformVersion = platformVersion;
+      _batteryLevel = batteryLevel;
     });
   }
 
@@ -48,7 +57,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('Running on: $_batteryLevel\n'),
         ),
       ),
     );
--- lib/pluginhello.dart
+++ lib/pluginhello.dart
@@ -2,12 +2,24 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+//プロジェクトを作る
+//プロジェクトを作成する時に、--template=pluginを付与することで、plugin用のプロジェクトが作られます。
+//flutter create --org com.yamadaryo --template=plugin -i swift -a kotlin hogehoge
+//他のパラメータについての解説を載せておきます。
+//--org: organaizationを指定します。通常、Reverse domain nameで指定します
+//-i swift -a kotlin: デフォルトでプロジェクトのネイティブコードは、Objective-CとJavaで生成されますが、SwiftとKotlinを使いたい場合は、このオプションで指定します。
+
 class Pluginhello {
   static const MethodChannel _channel = const MethodChannel('pluginhello');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> get batteryLevel async {
+    final String level = await _channel.invokeMethod('getBatteryLeval');
+    return level;
   }
 }
 
